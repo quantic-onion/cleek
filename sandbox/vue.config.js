0a094a6b6cc5b2c843db7fa5300a0a8c89127c83
LisandroDAS <lisandromdc@digitalairstrike.com>
2023-12-18 20:39:50 -0300

    remove old sandboxes

--- sandbox/vue.config.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  chainWebpack(config) {
-    config.resolve.symlinks(false)
-    config.resolve.alias.set('vue', path.resolve('./node_modules/vue'))    
-  },
-};
---
+++ sbox/readme.sandbox.md
@@ -0,0 +1,4 @@
+go to /pacakge folder and run <yarn link>
+go to /sbox folder and run <yarn link cleek>
+
+in order to see updated, you need to run <yarn build> on the package directory
