290f452bc8b5d64fb2a341dbe03eede3285a20fc
LisandroDAS <lisandromdc@digitalairstrike.com>
2023-12-19 14:48:31 -0300

    sandox: test popup with input date

---
+++ sbox/src/views/test-components/test-popup/TestPopup.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+// components
+import TestPopupWithInputDate from './TestPopupWithInputDate.vue';
+
+const isPopupActive = ref({
+  inputDate: false,
+});
+</script>
+
+<template>
+<TestPopupWithInputDate v-model="isPopupActive.inputDate"/>
+<ck-button @click="isPopupActive.inputDate = true">
+  Test popup with input date
+</ck-button>
+</template>
---
+++ sbox/src/views/test-components/test-popup/TestPopupWithInputDate.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+
+const props = defineProps<{
+  modelValue: boolean;
+}>();
+
+const emits = defineEmits<{
+  (e: 'update:modelValue', value: boolean): void;
+}>();
+
+const dateValue = ref(false);
+
+const isPopupActive = computed({
+  get() { return props.modelValue; },
+  set(val) { emits('update:modelValue', val); },
+});
+</script>
+
+<template>
+  
+<ck-popup v-model="isPopupActive" title="Test popup with input date">
+  <ck-input-date v-model="dateValue"/>
+</ck-popup>
+</template>
