8f3f92a81751319822e17ef91d3ce605801d0bf7
lisandromdc <lisandromdc@gmail.com>
2024-04-17 12:46:53 -0300

    sbox | test select

---
+++ sbox/src/views/test-components/TestSelect.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const options = [
+  { id: 1, name: 'Lisandro' },
+  { id: 2, name: 'Colo' },
+  { id: 3, name: 'Dino' },
+]
+const selectedOption = ref(null);
+</script>
+
+<template>
+  ESTE ES EL TEST DE LOS SELECTS
+<ck-select
+  v-model="selectedOption"
+  :options="options"
+  placeholder="Seleccione una opción"
+/>
+</template>
