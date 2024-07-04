70ad7b231922489eb155f6a090649270e35b6ead
Gianfranco Raselli <gianrase@hotmail.com>
2024-07-04 20:04:36 +1000

    sandbox config

--- sbox/src/main.ts
+++ sbox/src/main.ts
@@ -1,28 +1,24 @@
 import { createApp } from "vue";
-import App from "@/App.vue";
-// import i18n from "@/i18n/i18n";
-// import { createPinia } from "pinia";
+import App from "./App.vue";
+// pinia
+import { createPinia } from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+// plugins
+import router from "./router";
 import cleek from "cleek";
+// resources
 import cleekConfig from "@/cleek-config";
 // styles
-import "../node_modules/cleek/dist/style.css";
-import { createPinia } from "pinia";
-import router from "./router";
+import "cleek/main.css";
 
+// create app
 const app = createApp(App);
-// i18n
-// app.use(i18n);
 // pinia
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
 app.use(pinia);
-// router
+// plugins
 app.use(router);
-// cleek
-app.use(cleek, cleekConfig);
-
+app.use(cleek as any, cleekConfig);
+// mount
 app.mount("#app");
-
-// import "@/assets/styles/main.styl";
-// import "animate.css";
