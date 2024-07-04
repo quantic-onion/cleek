5483c9443583ec8619b4e8344b82223d4b97df04
Gianfranco Raselli <gianrase@hotmail.com>
2024-07-04 20:11:58 +1000

    MainLayout

--- sbox/src/layouts/MainLayout.vue
+++ sbox/src/layouts/MainLayout.vue
@@ -1,29 +1,27 @@
 <script setup lang="ts">
-import { computed } from 'vue';
-import { storeToRefs } from 'pinia';
+import { computed } from "vue";
+import { storeToRefs } from "pinia";
 // stores
-import { useLayoutStore } from '@/stores/layout.stores.ts';
+import { useLayoutStore } from "@/stores/layout.stores.ts";
 
 const { isDarkModeActive } = storeToRefs(useLayoutStore());
 
 const computedStyle = computed(() => {
   const list = [];
-  if (isDarkModeActive.value) list.push({ backgroundColor: '#23282E' });
+  if (isDarkModeActive.value) list.push({ backgroundColor: "#23282E" });
   return list;
 });
 </script>
 
 <template>
-<div class="main-app" :style="computedStyle">
-  <ck-navbar class="main-app--header">
-    <ck-switch v-model="isDarkModeActive">
-      DARK MODE
-    </ck-switch>
-  </ck-navbar>
-  <div class="main-app--content">
-    <router-view/>
+  <div class="main-app" :style="computedStyle">
+    <ck-navbar class="main-app--header">
+      <ck-switch v-model="isDarkModeActive"> DARK MODE </ck-switch>
+    </ck-navbar>
+    <div class="main-app--content">
+      <router-view />
+    </div>
   </div>
-</div>
 </template>
 
 <style lang="stylus" scoped>
