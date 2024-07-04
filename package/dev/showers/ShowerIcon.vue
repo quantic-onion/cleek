e236f96379c3b1652669471071be8258b8daa671
Gianfranco Raselli <gianrase@hotmail.com>
2024-07-04 19:24:28 +1000

    remove useless files

--- package/dev/showers/ShowerIcon.vue
+++
@@ -1,5 +0,0 @@
-<template lang="pug">
-ElementsShower(title="Íconos" subtitle="ck-icon")
-  ck-icon(spin icon="spinner")
-  ck-icon(icon="helicopter")
-</template>
--- package/dev/showers/ShowerLabel.vue
+++
@@ -1,7 +0,0 @@
-<template lang="pug">
-ElementsShower(title="Etiqueta" subtitle="ck-label")
-  ck-label
-    | label-example
-  ck-label(align="right")
-    | Jerardo Sofovich
-</template>
