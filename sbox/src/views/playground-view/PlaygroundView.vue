70a815e61f79064747ef70ddaba0b6ba04f747a2
Gianfranco Raselli <gianrase@hotmail.com>
2024-10-02 12:14:51 -0300

    computed

--- sbox/src/views/playground-view/PlaygroundView.vue
+++ sbox/src/views/playground-view/PlaygroundView.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import {  } from 'cleek';
+import { CkInput } from 'cleek';
 
-const value = ref(true);
+const value = ref(0);
 </script>
 
 <template>
   <div class="playground-view">
-    
+    <ck-input v-model="value" type="number" />
   </div>
 </template>
 
