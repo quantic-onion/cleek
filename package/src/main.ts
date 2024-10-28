4f1566c987d780167bb929bdf986c25ceb437156
Gianfranco Raselli <gianrase@hotmail.com>
2024-10-28 15:51:05 -0300

    main

--- package/src/main.ts
+++ package/src/main.ts
@@ -1,24 +1,23 @@
-import { App, Plugin } from 'vue';
+import { App } from 'vue';
+// plugins
+import vClickOutside from 'click-outside-vue3';
+import FloatingVue from 'floating-vue';
+// font awesome
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 // cleek-options
 import type { CleekOptions } from './cleek-options/cleek-options.types';
 import { defaultCleekOptions } from './cleek-options/default-cleek-options';
 import { useCleekOptionsStore } from './cleek-options/cleek-options.store';
 // components
 import * as components from './components';
-// fontawesome
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-// datepicker
-import '@vuepic/vue-datepicker/dist/main.css';
-// click-outside
-import vClickOutside from 'click-outside-vue3';
-// floating-vue
-import FloatingVue from 'floating-vue';
+// styles
 import 'floating-vue/dist/style.css';
+import '@vuepic/vue-datepicker/dist/main.css';
 
-// fontawesome
+// font awesome
 library.add(fas);
 library.add(far);
 library.add(fab);
@@ -43,16 +42,17 @@ function getCleekOptions(userOptions: CleekOptions) {
 }
 
 // install function executed by Vue.use()
-const install: Exclude<Plugin['install'], undefined> = function installCleek(app: App, options: CleekOptions) {
+function install(app: App, options: CleekOptions) {
+  // cleek options
   useCleekOptionsStore().cleekOptions = getCleekOptions(options);
   // plugins
   app.use(vClickOutside);
   app.use(FloatingVue);
-  // vue components
+  // components
   Object.entries(components).forEach(([componentName, component]) => {
     app.component(componentName, component);
   });
-};
+}
 
 // exports
 export default install;
