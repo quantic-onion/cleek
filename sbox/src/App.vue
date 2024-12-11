04abb0c430a964f1da3a55919701339b01f15ff1
Natalia Andrea Lossada <natilossada@hotmail.com>
2024-12-11 17:11:31 -0300

    add montserrat download fonts

--- sbox/src/App.vue
+++ sbox/src/App.vue
@@ -1,10 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
 
 <template>
   <router-view />
 </template>
 
 <style lang="stylus">
+@import '../../package/src/styles/index.styl';
 body
   margin 0
   font-family 'Montserrat', sans-serif
