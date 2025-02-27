043629b7962e968ae99b962b53783443306b16e0
Gianfranco Raselli <gianrase@hotmail.com>
2025-02-27 14:14:15 -0300

    ck-switch-options

--- package/src/components/ck-switch-options.vue
+++ package/src/components/ck-switch-options.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { computed, watch } from 'vue';
+import { computed } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 // components
 import CkLabel from './ck-label.vue';
 // hooks
@@ -8,21 +9,26 @@ import useWindowWidth from '../hooks/windowWidth';
 // types
 import type { Align } from '../cleek-options/cleek-options.types';
 
-type Option = {
-  id: boolean | number | string;
-  name: string;
-  icon?: string;
-};
+type OptionValue = any;
+type Option = any;
 
-const selectedOption = defineModel<Option['id']>({ required: true });
+const valueSelected = defineModel<OptionValue>({ required: true });
 
 const props = withDefaults(
   defineProps<{
     options?: Option[];
-    // reduce functions
-    prop?: string;
+    queryName?: string;
     notReduce?: boolean;
-    reduceFunction?: (option: Option) => any;
+    // reduce value
+    reduceValueProp?: string;
+    reduceValueMethod?: string;
+    reduceValueFunction?: (option: Option) => OptionValue;
+    notReduceValue?: boolean;
+    // reduce name
+    reduceNameProp?: string;
+    reduceNameMethod?: string;
+    reduceNameFunction?: (option: Option) => string;
+    notReduceName?: boolean;
     // label
     label?: string;
     labelAlign?: Align;
@@ -34,117 +40,135 @@ const props = withDefaults(
     sameWidthOptions?: boolean;
   }>(),
   {
-    prop: 'name',
+    reduceValueProp: 'id',
+    reduceNameProp: 'name',
   },
 );
 
 const emit = defineEmits<{
-  change: [value: Option];
+  change: [val: OptionValue];
 }>();
 
+const route = useRoute();
+const router = useRouter();
 const { windowWidth } = useWindowWidth();
 
-const computedClass = computed(() => {
+const isUsingQuery = computed(() => !!props.queryName);
+const optionsClass = computed(() => {
   const list = [];
-  // group
   list.push(functions.getGroupClass(props, windowWidth.value));
   return list;
 });
-const computedItemStyle = computed(() => {
+const optionStyle = computed(() => {
   const list = [];
   if (props.sameWidthOptions) list.push({ width: `${100 / (props.options || []).length}%` });
   return list;
 });
 
-watch(() => selectedOption.value, (val) => emit('change', val));
-
-function defaultReduceFunction(option) {
-  return option.id;
-}
 function getOptionValue(option: Option) {
-  if (props.notReduce) return option.name;
-  if (props.reduceFunction) {
-    return props.reduceFunction(option);
-  }
-  return defaultReduceFunction(option);
+  if (props.reduceValueFunction) return props.reduceValueFunction(option);
+  if (props.notReduceValue || props.notReduce) return option;
+  if (props.reduceValueMethod) return option[props.reduceValueMethod]();
+  return option[props.reduceValueProp];
+}
+function getOptionName(option: Option) {
+  if (props.reduceNameFunction) return props.reduceNameFunction(option);
+  if (props.notReduceName || props.notReduce) return option;
+  if (props.reduceNameMethod) return option[props.reduceNameMethod]();
+  return option[props.reduceNameProp];
+}
+function handleOptionClick(option: Option) {
+  const value = getOptionValue(option);
+  valueSelected.value = value;
+  emit('change', value);
+  if (isUsingQuery.value) router.push({ query: { [props.queryName]: value } });
+}
+function initValueSelected() {
+  if (!isUsingQuery.value) return;
+  const queryValue = route.query[props.queryName] as OptionValue;
+  if (!queryValue) return;
+  valueSelected.value = queryValue;
 }
 
+// init component
+initValueSelected();
 </script>
 
 <template>
-  <div class="ck-switch-options__container-exterior">
+  <div class="ck-switch-options">
     <!-- label -->
     <ck-label v-if="label" :align="labelAlign">
       {{ label }}
     </ck-label>
-    <!-- switch options -->
-    <div class="ck-switch-options__container" :class="computedClass">
+    <!-- options -->
+    <div class="switch-options--options" :class="optionsClass">
       <div
         v-for="(option, index) in options"
-        class="ck-switch-options__option"
-        :key="`ck-switch-options${index}`"
-        :class="{ selected: selectedOption == getOptionValue(option) }"
-        :style="computedItemStyle"
-        @click="selectedOption = getOptionValue(option)"
+        :key="index"
+        class="options--option"
+        :class="{ selected: getOptionValue(option) === valueSelected }"
+        :style="optionStyle"
+        @click="handleOptionClick(option)"
       >
-        <ck-icon v-if="option.icon" class="ck-switch-options__option--icon" :icon="option.icon" />
-        {{ option[prop] }}
+        <ck-icon v-if="option.icon" class="option--icon" :icon="option.icon" />
+        {{ getOptionName(option) }}
       </div>
     </div>
   </div>
 </template>
 
 <style lang="stylus" scoped>
-$borderWidth = 2px
-$borderRadius = 5px
-$borderColor = var(--primary)
-// $borderColor = rgba(var(--primary), .45) FIXRGBA
-.ck-switch-options__container
-  overflow hidden
-  border $borderWidth solid $borderColor
-  border-radius $borderRadius
-  width 100%
-  display flex
-  align-items stretch
-  flex-wrap wrap
-  min-height $globalMinHeight
-  .ck-switch-options__option
-    border-right $borderWidth solid $borderColor
-    display inline-flex
-    justify-content center
-    align-items center
-    text-align center
-    my-box-shadow()
-    background-color white
-    border-radius 0
-    text-align center
-    font-weight 500
-    color #666
-    padding 5px 15px
-    flex-grow 1
-    transition transform 0.2s
-    &:hover
-      cursor pointer
-      // background-color rgba(var(--primary), .1) FIXRGBA
-    &:last-of-type
-      border-right 0
-    &:hover
-      opacity 0.9
-    &.selected
-      background-color var(--primary)
-      color white
-    .ck-switch-options__option--icon
-      margin-right 0.4rem
-// group
-.group-top
-  &.ck-switch-options__container
-    border-radius-bottom 0
-  .ck-switch-options__option
-    border-radius 0 !important
-.group-bottom
-  &.ck-switch-options__container
-    border-top 0
-    border-radius-top 0
-  .ck-switch-options__option
-    border-radius 0 !important
+.ck-switch-options
+  $borderWidth = 2px
+  $borderRadius = 5px
+  $borderColor = var(--primary)
+  // $borderColor = rgba(var(--primary), .45) FIXRGBA
+  .switch-options--options
+    overflow hidden
+    border $borderWidth solid $borderColor
+    border-radius $borderRadius
+    width 100%
+    display flex
+    align-items stretch
+    flex-wrap wrap
+    min-height $globalMinHeight
+    .options--option
+      border-right $borderWidth solid $borderColor
+      display inline-flex
+      justify-content center
+      align-items center
+      text-align center
+      my-box-shadow()
+      background-color white
+      border-radius 0
+      text-align center
+      font-weight 500
+      color #666
+      padding 5px 15px
+      flex-grow 1
+      transition transform 0.2s
+      &:hover
+        cursor pointer
+        // background-color rgba(var(--primary), .1) FIXRGBA
+      &:last-of-type
+        border-right 0
+      &:hover
+        opacity 0.9
+      &.selected
+        background-color var(--primary)
+        color white
+      .option--icon
+        margin-right 0.4rem
+  // group
+  .group-top
+    &.switch-options--options
+      border-radius-bottom 0
+    .options--option
+      border-radius 0 !important
+  .group-bottom
+    &.switch-options--options
+      border-top 0
+      border-radius-top 0
+    .options--option
+      border-radius 0 !important
 </style>
